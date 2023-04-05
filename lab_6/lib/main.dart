2f792f5dbcc6ea0ffc31b019a14831df0d84e8fe
Nikeshhh <WityazKajit@gmail.com>
2023-04-05 13:12:40 +0300

    Добавлен валидатор значения поля формы

--- lab_6/lib/main.dart
+++ lab_6/lib/main.dart
@@ -13,10 +13,41 @@ class MyForm extends StatefulWidget{
 
 class MyFormState extends State {
   final _formKey = GlobalKey<FormState>();
+  double _width = 0;
+  double _height = 0;
+
+  bool _myValidator(val){
+    try{
+      double.parse(val);
+      return true;
+    }
+    on Exception {
+      return false;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container()
+    return Container(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Row( // Первая строка ввода
+              children: [
+                const Text('Ширина (мм): '),
+                TextFormField(validator: (value){
+                  if (value!.isEmpty || )
+                })
+              ],
+            ),
+            Row( // Вторая строка ввода
+
+            )
+          ],
+        ),
+      ),
+    )
   }
 }
 
