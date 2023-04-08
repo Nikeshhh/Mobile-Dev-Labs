c50f6a0681ddcf3d8c36a24417473106e58488df
Nikeshhh <WityazKajit@gmail.com>
2023-04-08 18:25:34 +0300

    Добавлен виджет для вывод изображений

--- lab_8/lib/main.dart
+++ lab_8/lib/main.dart
@@ -38,3 +38,20 @@ class Photo {
     );
   }
 }
+
+class PhotosList extends StatelessWidget {
+  const PhotosList({Key? key, required this.photos}) : super(key: key);
+
+  final List<Photo> photos;
+
+  @override
+  Widget build(BuildContext context){
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemCount: photos.length,
+      itemBuilder: (context, index) {
+        return Image.network(photos[index].thumbnailUrl);
+    },
+    );
+  }
+}
