6755e434c5d974e63e34cc44263999a4c0dc5585
Nikeshhh <WityazKajit@gmail.com>
2023-04-12 11:50:35 +0300

    Добавлен класс кофемашины, созданы поля, геттеры, сеттеры

---
+++ lab_9/lib/classes/coffee_machine.dart
@@ -0,0 +1,39 @@
+
+class Machine {
+  late final int _coffeeBeans;
+  late final int _milk;
+  late final int _water;
+  late final int _cash;
+
+  int getCoffeeBeans () {
+    return _coffeeBeans;
+}
+
+  int getMilk () {
+    return _milk;
+  }
+
+  int getWater () {
+    return _water;
+  }
+
+  int getCash () {
+    return _cash;
+  }
+
+  void setCoffeeBeans (int value) {
+    _coffeeBeans = value;
+  }
+
+  void setMilk (int value) {
+    _milk = value;
+  }
+
+  void setWater (int value) {
+    _water = value;
+  }
+
+  void setCash (int value) {
+    _cash = value;
+  }
+}
